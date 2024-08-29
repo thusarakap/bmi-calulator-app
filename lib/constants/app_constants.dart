f9f31e44124c2e8ee54221b13dfd34322f961301
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-08-30 01:33:03 +0530

    Create app_constants.dart

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#474787");
+final Color accentHexColor = HexColor("#AAABB8");
